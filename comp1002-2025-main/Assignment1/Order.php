4cc1c1590bd3612a3e73861abcaa5e1d934ac818
Mateo Grueso <mateogrueso10@gmail.com>
2025-11-02 00:43:39 +0000

    added the pizza order form

--- comp1002-2025-main/Assignment1/Order.php
+++ comp1002-2025-main/Assignment1/Order.php
@@ -43,3 +43,48 @@
         <label for="delivery-date">Preferred Delivery Date:</label>
         <input type="date" id="delivery-date" name="delivery-date" />
       </fieldset>
+
+      <!-- PIZZA DETAILS -->
+      <fieldset>
+        <legend>Pizza Details</legend>
+
+        <label for="num-pizzas">Number of Pizzas:</label>
+        <input type="number" id="num-pizzas" name="num-pizzas" min="1" max="10" value="1" />
+
+        <label for="size">Pizza Size:</label>
+        <select id="size" name="size">
+          <option value="small">Small (6 slices)</option>
+          <option value="medium">Medium (8 slices)</option>
+          <option value="large">Large (12 slices)</option>
+        </select>
+
+        <label>Pizza Shape:</label>
+        <input type="radio" id="round" name="shape" value="round" checked />
+        <label for="round">Round</label>
+        <input type="radio" id="square" name="shape" value="square" />
+        <label for="square">Square</label>
+
+        <label>Crust Type:</label>
+        <input type="radio" id="thin" name="crust" value="thin" checked />
+        <label for="thin">Thin Crust</label>
+        <input type="radio" id="NY" name="crust" value="NY" checked />
+        <label for="thin">New York Style</label>
+        <input type="radio" id="deep" name="crust" value="deep" />
+        <label for="deep">Deep Dish</label>
+        <input type="radio" id="stuffed" name="crust" value="stuffed" />
+        <label for="stuffed">Stuffed Crust</label>
+        <input type="radio" id="Cauliflower" name="crust" value="Cauliflower" />
+        <label for="stuffed">Cauliflower Crust (Gluten Free)</label>
+
+        <label>Toppings:</label>
+        <div>
+          <input type="checkbox" id="pepperoni" name="toppings[]" value="pepperoni" /> <label for="pepperoni">Pepperoni</label><br>
+          <input type="checkbox" id="mushrooms" name="toppings[]" value="mushrooms" /> <label for="mushrooms">Mushrooms</label><br>
+          <input type="checkbox" id="onions" name="toppings[]" value="onions" /> <label for="onions">Onions</label><br>
+          <input type="checkbox" id="bacon" name="toppings[]" value="bacon" /> <label for="bacon">Bacon</label><br>
+          <input type="checkbox" id="pineapple" name="toppings[]" value="pineapple" /> <label for="pineapple">Pineapple</label>
+        </div>
+
+        <label for="special">Special Instructions:</label>
+        <textarea id="special" name="special" rows="3" placeholder="Example: no onions, extra cheese..."></textarea>
+      </fieldset>
