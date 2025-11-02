0c30604fda3dd8ebb14e83be5d434ca13061994a
Mateo Grueso <mateogrueso10@gmail.com>
2025-11-02 00:18:23 +0000

    added the title and paragraphs to the order form

---
+++ comp1002-2025-main/Assignment1/Order.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Mateo’s Pizza | Order Now</title>
+  <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+  <header>
+    <h1>Order Your Pizza</h1>
+  </header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="Menu.php">Menu</a></li>
+                <li><a href="Order.php">Order Form</a></li>
+                <li><a href="#about">About Us</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+ <main>
+    <h2>Customize Your Perfect Pizza!</h2>
+    <p>Fill out the form below to build your dream pizza. We’ll get it ready fresh and fast!</p>
