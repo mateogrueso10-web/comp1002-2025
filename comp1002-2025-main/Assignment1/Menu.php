f4c2d9ede7c34e2fe3925220709a6c79768a6d31
Mateo Grueso <mateogrueso10@gmail.com>
2025-11-02 00:08:56 +0000

    added classic pizza options

---
+++ comp1002-2025-main/Assignment1/Menu.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mat's Pizza Menu</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Menu</h1>
+        <nav>
+            <ul>
+                <!--the links to go to each page-->
+                <li><a href="index.php">Home</a></li>
+                <li><a href="Menu.php">Menu</a></li>
+                <li><a href="#about">About Us</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section id="Menu">
+<main>
+    <h2>Our Most Popular Pizzas</h2> <!-- give them some options-->
+    <section>
+      <h3>Classic Pizzas</h3>
+      <ul>
+        <li><strong>Margherita</strong> – Tomato sauce, mozzarella, and basil</li>
+        <li><strong>Pepperoni</strong> – Spicy pepperoni and mozzarella</li>
+        <li><strong>Hawaiian</strong> – Ham, pineapple, and mozzarella</li>
+      </ul>
+    </section>
+</main>
+</body>
