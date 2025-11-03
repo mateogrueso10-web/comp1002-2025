c449e95e42450da36603fa90d9c3ecd5e9544d5f
Mateo Grueso <mateogrueso10@gmail.com>
2025-11-03 01:36:42 +0000

    removed some unnesscary stuff

--- comp1002-2025-main/Assignment1/index.php
+++ comp1002-2025-main/Assignment1/index.php
@@ -16,7 +16,6 @@
                 <li><a href="index.php">Home</a></li>
                 <li><a href="Menu.php">Menu</a></li>
                 <li><a href="Order.php">Order Form</a></li>
-                <li><a href="#about">About Us</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
